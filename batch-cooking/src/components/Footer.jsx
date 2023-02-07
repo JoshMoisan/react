fcd5f2b8e8b2a6261aa8cf8d5e6734e865010951
joshmoisan <josh.moisan@hotmail.fr>
2023-02-07 10:48:08 -0500

    mardi am tuneup

--- batch-cooking/src/components/Footer.jsx
+++ batch-cooking/src/components/Footer.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSnapchat } from '@fortawesome/free-brands-svg-icons'
+
 
 const Footer = () => {
 
   return(
-    <div className="footer">
-      <div>
-        <h3>BatchCooking made fun</h3>
-      </div>
+    <div>
+      <div className="footer">
+        <div>
+          <h3>BatchCooking made fun !</h3>
+        </div>
 
-      <span>
-        <ul>
-          <li><a href="https://www.linkedin.com/in/josh-moisan-webdev/" target="_blank">Contact Us </a></li>
-          <li><a href="/oops">Career </a></li>
-          <li><a href="/oops">Connect with us</a></li>
-        </ul>
-      </span>
-    </div>
+        <div>
+          <ul>
+            <li><a href="https://www.linkedin.com/in/josh-moisan-webdev/" target="_blank">Contact Us </a></li>
+            <li><a href="/oops">Career </a></li>
+            <li><a href="/oops">Connect with us</a></li>
+          </ul>
+        </div>
+
+      </div>
+        <div className="connect-social">
+          <span><FontAwesomeIcon icon={faFacebook} /></span>
+          <span><FontAwesomeIcon icon={faInstagram} /></span>
+          <span><FontAwesomeIcon icon={faSnapchat} /></span>
+        </div>
+      </div>
   )
 }
 
